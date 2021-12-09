29b1baea3edc2ea0eeb7754f2c482e1b6fe006ca
gwdixon <93284217+gwdixon@users.noreply.github.com>
2021-12-09 14:10:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
 import random
+import math
+
+_author_ = "Garrett Dixon"
 
 # Garrett Dixon
 # this program guides the user through a number of operations in the form
@@ -26,33 +28,32 @@ print("you have encountered an enemy")
 
 
 # attack function
-def swing(funcplayerhp, funcenemyhp, funclevel, funcdamagemulti,
-          funcbasedamage):
+def swing(func_player_hp, func_enemy_hp, func_level, func_damage_multi,
+          func_base_damage):
     dead = 0
-    funcenemyhp = funcenemyhp * funclevel
-    while dead == 0:
-        funcenemyhp -= (random.randrange(funcbasedamage, funcbasedamage + 5)
-                        ** funcdamagemulti) // 1
-        funcplayerhp -= random.randrange(0 + funclevel, 2 + funclevel) * \
-                        funclevel
-        print("the enemy hp is now", funcenemyhp)
-        print(name + "'s hp is now", funcplayerhp)
-        if funcplayerhp <= 0:
-            dead = 1
+    func_enemy_hp = func_enemy_hp * func_level
+    while not dead:
+        func_enemy_hp -= (random.randrange(func_base_damage,
+                                           func_base_damage + 5)
+                          ** func_damage_multi) // 1
+        func_player_hp -= random.randrange(0 + func_level, 2 + func_level) * \
+                          func_level
+        print("the enemy hp is now", func_enemy_hp)
+        print(name + "'s hp is now", func_player_hp)
+        if func_player_hp <= 0:
             return 0
-        if funcenemyhp <= 0:
-            dead = 1
+        if func_enemy_hp <= 0:
             return 1
 
 
 # finds if the player won or lost the battle and responds accordingly
-def afterBattle(funcsuccess, funcgold, funclevel):
-    if funcsuccess == 1:
+def afterBattle(func_success, func_gold, func_level):
+    if func_success == 1:
         print("Congratulations!")
-        funcgold += random.randrange(50, 150) * funclevel
+        func_gold += random.randrange(50, 150) * func_level
         print("You defeated the enemy")
-        print("You now have", funcgold, "gold")
-        return funcgold
+        print("You now have", func_gold, "gold")
+        return func_gold
     else:
         print("Game Over!" * 10)
         print("You lost so bad that I said it 10 times!")
@@ -75,52 +76,57 @@ while attackLoop != 1:
 
 print("Congratulations on defeating your first enemy!")
 # this line thinks is sql for some reason
-print("Select the shop from the location menu")
+print("select the shop from the location menu")
 
 
 # controls the menu for locations
 def locationMenu():
+    """
+
+    :return:
+    """
+    loc_name = ""
     for location in range(1, 5):
         if location == 1:
-            locName = "fight"
+            loc_name = "fight"
         elif location == 2:
-            locName = "shop"
+            loc_name = "shop"
         elif location == 3:
-            locName = "minigame"
+            loc_name = "minigame"
         elif location == 4:
-            locName = "Quit Game"
-        print(location, ".", locName)
-    funcdestination = input("chose the number where you want to go")
+            loc_name = "Quit Game"
+        print(location, ".", loc_name)
+    func_destination = input("chose the number where you want to go")
     try:
-        funcdestination = int(funcdestination)
+        func_destination = int(func_destination)
     except:
         print("Invalid selection. Try again.")
         return 5
-    if funcdestination > 4:
+    if func_destination > 4:
         print("Invalid selection. Try again.")
         return 5
-    if funcdestination < 1:
+    if func_destination < 1:
         print("Invalid selection. Try again.")
         return 5
-    if funcdestination == 1:
+    if func_destination == 1:
         return 0
-    if funcdestination == 2:
+    if func_destination == 2:
         return 1
-    if funcdestination == 3:
+    if func_destination == 3:
         return 2
-    if funcdestination == 4:
+    if func_destination == 4:
         return 3
 
 
-def shopMenu(funcupgrade1, funcupgrade2, funcupgrade3, funcgold):
-    funccost1 = int((200 ** math.log2(funcupgrade1 * 1.01)) // 1)
-    funccost2 = 75 * (funcupgrade2 ** 1.002) // 1
-    funccost3 = 1000 * (funcupgrade3 ** 1.001) // 1
-    print("1. Upgrade your base damage for", funccost1)
-    print("2. Increase your base hit points for", funccost2)
-    print("3. Increase your damage multiplier for", funccost3)
+def shopMenu(func_upgrade1, func_upgrade2, func_upgrade3, func_gold):
+    func_cost1 = int((200 ** math.log2(func_upgrade1 * 1.01)) // 1)
+    func_cost2 = 75 * (func_upgrade2 ** 1.002) // 1
+    func_cost3 = 1000 * (func_upgrade3 ** 1.001) // 1
+    print("1. Upgrade your base damage for", func_cost1)
+    print("2. Increase your base hit points for", func_cost2)
+    print("3. Increase your damage multiplier for", func_cost3)
     print("4. Leave")
-    print("You currently have", funcgold, "gold")
+    print("You currently have", func_gold, "gold")
     choice = input()
     try:
         choice = int(choice)
@@ -134,19 +140,19 @@ def shopMenu(funcupgrade1, funcupgrade2, funcupgrade3, funcgold):
         print("Invalid selection. Try again.")
         return 4
     if choice == 1:
-        if funcgold < funccost1:
+        if func_gold < func_cost1:
             print("you don't have enough gold for that upgrade")
             shopMenu(upgrade1, upgrade2, upgrade3, gold)
         else:
             return 0
     if choice == 2:
-        if funcgold < funccost2:
+        if func_gold < func_cost2:
             print("you don't have enough gold for that upgrade")
             shopMenu(upgrade1, upgrade2, upgrade3, gold)
         else:
             return 1
     if choice == 3:
-        if funcgold < funccost3:
+        if func_gold < func_cost3:
             print("you don't have enough gold for that upgrade")
             shopMenu(upgrade1, upgrade2, upgrade3, gold)
         else:
@@ -156,6 +162,7 @@ def shopMenu(funcupgrade1, funcupgrade2, funcupgrade3, funcgold):
 
 
 def miniGame():
+    modeName = ""
     for mode in range(1, 4):
         if mode == 1:
             modeName = "Easy"
@@ -180,8 +187,8 @@ def miniGame():
         print("Invalid selection. Try again.", end="")
         miniGame()
     if difficulty == 1:
-        wrongloop = 0
-        while wrongloop == 0:
+        wrong_loop = 0
+        while wrong_loop == 0:
             guess = input("try to guess the same number as the computer. The"
                           " computer will guess 1 or 2")
             # checks to make sure number entered is valid
@@ -194,7 +201,7 @@ def miniGame():
                     guess = int(guess)
                     print("Invalid selection. Try again.")
                 if guess == 1 or 2:
-                    wrongloop = 1
+                    wrong_loop = 1
                     if (guess % 2) == 0:
                         print("you chose an even number")
                     if guess == 1 or guess == 2:
@@ -203,11 +210,11 @@ def miniGame():
             except:
                 print("Invalid selection. Try again.")
     if difficulty == 2:
-        wrongloop = 0
-        while wrongloop == 0:
+        wrong_loop = 0
+        while wrong_loop == 0:
             guess = input("try to guess the same number as the computer. The"
                           " computer will guess 1 or 2")
-            computerguess = random.randrange(1, 3)
+            computer_guess = random.randrange(1, 3)
             # checks to make sure number entered is valid
             try:
                 guess = int(guess)
@@ -218,19 +225,19 @@ def miniGame():
                     guess = int(guess)
                     print("Invalid selection. Try again.")
                 if guess == 1 or 2:
-                    wrongloop = 1
-                    if guess != (not computerguess):
+                    wrong_loop = 1
+                    if guess != (not computer_guess):
                         print("You win!")
                     else:
                         print("You Lost!")
-                        print("the computer chose", computerguess)
+                        print("the computer chose", computer_guess)
 
 
             except:
                 print("Invalid selection. Try again.")
     if difficulty == 3:
-        wrongloop = 0
-        while wrongloop == 0:
+        wrong_loop = 0
+        while wrong_loop == 0:
             guess = input("try to guess the same number as the computer. The"
                           " computer will guess 1 or 2")
             # checks to make sure number entered is valid
@@ -243,7 +250,7 @@ def miniGame():
                     guess = int(guess)
                     print("Invalid selection. Try again.")
                 if guess == 1 or 2:
-                    wrongloop = 1
+                    wrong_loop = 1
                     if guess == 1 and guess == 2:
                         print("You win!")
                     else:
@@ -274,7 +281,7 @@ while shopLoop == 0:
             cost2 = 75 * (upgrade2 ** 1.002) // 1
             gold -= cost2
             upgrade2 += 1
-            playerHP = playerHP * 2
+            playerHP *= 2
             print("Your hit points is now", playerHP)
         elif upgrade == 2:
             cost3 = 1000 * (upgrade3 ** 1.001) // 1
